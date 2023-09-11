ed1d82862a072bbe6f59fbfc6d8e17158edd0231
Majid-Mohammed <abdulmajidmohammed71@gmail.com>
2023-09-11 14:18:12 -0700

    except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	/* the loop to print the  latters */
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
