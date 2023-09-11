81b91cd4fb0bdf674ec8cebda1f65108411270da
Majid-Mohammed <abdulmajidmohammed71@gmail.com>
2023-09-11 14:26:35 -0700

    10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	/* the loop to print the  latters */
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	return (0);
+}
