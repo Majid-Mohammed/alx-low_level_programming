7ac55ac4d68bf551c5aa4919512a36eaea3157bc
Majid-Mohammed <abdulmajidmohammed71@gmail.com>
2023-09-12 05:42:54 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* code discription */
+	printf("_putchar\n");
+	return (0);
+}
