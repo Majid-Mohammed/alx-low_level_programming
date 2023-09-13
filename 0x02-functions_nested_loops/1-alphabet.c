6141fef031783e4f19b52018bc09c0d39fad3525
Majid-Mohammed <abdulmajidmohammed71@gmail.com>
2023-09-13 13:22:28 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+}
+
+int main(void)
+{
+	/* my code*/
+	print_alphabet();
+	return (0);
+}
